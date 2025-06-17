639c16c4029ee4a48ca868db8f8f4cd7df2376d3
= <yudojago@gmail.com>
2025-06-17 22:10:34 +0700

    Changed backend host

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,10 @@ import app from './app.js'
 
 const start = async () => {
   try {
-    await app.listen({ port: process.env.PORT || 5000 })
+    await app.listen({
+      port: process.env.PORT || 5000,
+      host: '0.0.0.0',
+    })
     app.log.info(`listening on ${process.env.PORT || 5000}`)
     app.log.info(`running on ${process.env.NODE_ENV}`)
   } catch (err) {
